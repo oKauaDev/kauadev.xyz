50e796d4cddecf09e2a1220ef5efa63a7098b6bd
LadinoXx <kauacomtil021@gmail.com>
2022-11-07 10:58:47 -0300

    Site v4 BETA 1

--- js/main.js
+++ js/main.js
@@ -1,139 +1,48 @@
-const animateText = document.getElementById("subtitle-top");
+const navbarButton = document.getElementById("hamburger");
+const navbarDiv = document.querySelector("header .navbar");
 
-function writeText(element) {
-    const textToArray = element.innerHTML.split('');
-    element.innerHTML = "";
-    textToArray.forEach((letra, i) => {
-        setTimeout(function() {
-            element.innerHTML += letra;
-        }, 65 * i);
+function closeNavbar() {
+  if (navbarDiv.classList.contains("open")) {
+    navbarDiv.classList.remove("open");
+    document.documentElement.style.overflow = "auto";
+    document.body.scroll = "auto";
+    scrollTo(0, 0);
+  }
+}
+
+navbarButton.addEventListener("click", () => {
+  navbarDiv.classList.toggle("open");
+  if (navbarDiv.classList.contains("open")) {
+    document.documentElement.style.overflow = "hidden";
+    document.body.scroll = "no";
+    scrollTo(0, 0);
+
+    window.addEventListener("click", (event) => {
+      if (
+        event.target.tagName != "UL" &&
+        event.target.tagName != "LI" &&
+        event.target.tagName != "LABEL"
+      ) {
+        closeNavbar();
+      }
     });
-}
-
-writeText(animateText);
-
-//Lista de projetos interativa
-const listProjects = document.querySelectorAll("[project-id]");
-
-function DeselectAllProjects() {
-    listProjects.forEach(Lelement => {
-        Lelement.classList.remove('selectedProject');
-    })
-}
-
-const titleProject = document.getElementById("title-projeto");
-const descriptionProject = document.getElementById("description-projeto");
-refreshProjects("p-1")
-function refreshProjects(id) {
-    switch (id) {
-        case "p-1":
-            titleProject.innerHTML = "PlayScript"
-            descriptionProject.innerHTML = "Execute scripts em php dentro de seu servidor no minecraft, sem precisar de reiniciar ao algo do gênero! basta digitar um comando.";
-            break;
-
-        case "p-2":
-            titleProject.innerHTML = "EnableEncryptedPlugin"
-            descriptionProject.innerHTML = "Deixe seu plugin aprova de vazamentos usando EnableEncryptedPlugin! basta compilar para seu cliente utilizando o IP do servidor dele.";
-            break;
-
-        case "p-3":
-            titleProject.innerHTML = "Bedrock-community"
-            descriptionProject.innerHTML = "Um dos meus primeiros sites feitos sem JS.";
-            break;
-    }
-}
-
-listProjects.forEach(Lelement => {
-    Lelement.addEventListener('click', event => {
-        const target = event.target;
-        DeselectAllProjects();
-        target.classList.add('selectedProject');
-        refreshProjects(target.getAttribute("project-id"));
-    });
+  } else {
+    document.documentElement.style.overflow = "auto";
+    document.body.scroll = "auto";
+    scrollTo(0, 0);
+    window.removeEventListener("click");
+  }
 });
 
-/**
- * Ultima função de Js, a parte de lingugens
-*/
+const certificados = document.querySelectorAll(
+  "section.certificados .certificates .all-certificados .certificate-item"
+);
 
-const DescriptionLinguage = document.getElementById('description-linguage');
-if (window.innerWidth > 800) {
-    const LinguagensDivs = document.querySelectorAll(".linguagem-index-select");
-    LinguagensDivs.forEach(ElementoD => {
-        ElementoD.addEventListener("mouseenter", function(event) {
-            const target = event.target.parentElement;
-            switch (target.getAttribute('type-linguage')) {
-                case "css":
-                    DescriptionLinguage.innerHTML = "Ao falarmos sobre o que é CSS (Cascading Style Sheet, ou Folha de Estilo em Cascatas), é necessário dizer que sua aplicação é utilizada na estilização de componentes escritos em linguagens de marcação.";
-                    break;
-            
-                case "js":
-                    DescriptionLinguage.innerHTML = "O JavaScript é utilizado para controlar o HTML e o CSS, usado para manipular comportamentos na página. O que seria esse comportamento? Por exemplo, submenu, carousel, etc. Portanto, O JavaScript permite implementar itens complexos em páginas web.";
-                    break;
-            
-                case "html":
-                    DescriptionLinguage.innerHTML = "HTML (Linguagem de Marcação de Hipertexto) é o código que você usa para estruturar uma página web e seu conteúdo. Por exemplo, o conteúdo pode ser estruturado em parágrafos, em uma lista com marcadores ou usando imagens e tabelas.";
-                    break;
-            
-            }
-            const name = target.querySelector("p");
-            name.style.display = "block";
-        });
-        ElementoD.addEventListener('mouseout', function(event) {
-            if (event.target.classList.contains("linguagem-index")) {
-                const target = event.target;
-                const name = target.querySelector("p");
-                removeDescriptionLinguage(name);
-            }else{
-                const target = event.target.parentElement;
-                const name = target.querySelector("p");
-                removeDescriptionLinguage(name);
-            }
-        });
-    });
-}else{
-    linguagePromisse();
-}
+const certificateView = document.getElementById("certificado0");
 
-function removeDescriptionLinguage(element) {
-    element.style.display = "none";
-    DescriptionLinguage.innerHTML = "*Clique ou passe o mouse por cima para ver.";
-}
-
-//loop de código para mobile
-
-async function linguagePromisse() {
-    for (c = 0; true; c += 5) {
-        switch (c % 3) {
-            case 2:
-                DescriptionLinguage.innerHTML = "Ao falarmos sobre o que é CSS (Cascading Style Sheet, ou Folha de Estilo em Cascatas), é necessário dizer que sua aplicação é utilizada na estilização de componentes escritos em linguagens de marcação.";
-                break;
-        
-            case 1:
-                DescriptionLinguage.innerHTML = "O JavaScript é utilizado para controlar o HTML e o CSS, usado para manipular comportamentos na página. O que seria esse comportamento? Por exemplo, submenu, carousel, etc. Portanto, O JavaScript permite implementar itens complexos em páginas web.";
-                break;
-        
-            case 0:
-                DescriptionLinguage.innerHTML = "HTML (Linguagem de Marcação de Hipertexto) é o código que você usa para estruturar uma página web e seu conteúdo. Por exemplo, o conteúdo pode ser estruturado em parágrafos, em uma lista com marcadores ou usando imagens e tabelas.";
-                break; 
-        }
-        await new Promise(res => setTimeout(res, 10000));
-    }
-}
-
-//Modal contatos
-
-const modal = document.getElementById("base-modal");
-
-function openModal() {
-    closeNavbar();
-    modal.setAttribute("visible", "true");
-    modal.style.display = "block";
-}
-
-window.addEventListener('click', event => {
-    if (!event.target.classList.contains("modalopenbutton")) {
-        modal.setAttribute("visible", "false");
-        modal.style.display = "none";
-    }
+certificados.forEach((Element) => {
+  Element.addEventListener("mouseenter", (event) => {
+    let target = event.target;
+    certificateView.src = target.getAttribute("certificate");
+  });
 });
