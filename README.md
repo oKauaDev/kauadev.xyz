7d50790fd02992deec00c0f71c80927231163599
LadinoXx <kauacomtil021@gmail.com>
2022-12-03 10:22:09 -0300

    Test new Site

--- README.md
+++
@@ -1,5 +0,0 @@
-# SOBRE
-Este é apenas um portfolio que faço enquanto aprendo html e css.
-
-# MUDANÇAS
-Esse código estará em constante mudanças para facilitar meu aprendizado.
---
+++ script.js
@@ -0,0 +1,73 @@
+function initNavbarAnimation() {
+  const navbarLinks = document.querySelectorAll("header ul a");
+
+  let timeOutId = 0;
+  let isWrite = true;
+
+  navbarLinks.forEach((Item) => {
+    Item.addEventListener("mouseenter", HandleMouseEnter);
+    Item.addEventListener("mouseleave", HandleMouseLeave);
+  });
+
+  function HandleMouseEnter(event) {
+    isWrite = true;
+    const target = event.currentTarget;
+    let letras = "(x)".split("");
+    if (window.innerWidth >= 800) {
+      letras = "(select)".split("");
+    }
+    letras.forEach((letra, i) => {
+      timeOutId = setTimeout(() => {
+        if (isWrite) target.innerText += letra;
+      }, i * 35);
+    });
+  }
+
+  function HandleMouseLeave(event) {
+    isWrite = false;
+    clearTimeout(timeOutId);
+    const target = event.currentTarget;
+    target.innerText = target.getAttribute("data-baseText");
+  }
+}
+
+function initNavbar() {
+  const btnOpenNav = document.querySelector("header label");
+  const header = document.querySelector("header");
+  const allLinksNavbar = document.querySelectorAll("header ul a");
+  const ulNavbar = document.querySelector("header ul");
+  ulNavbar.style.height = document.body.clientHeight + "px";
+  allLinksNavbar.forEach((Link) => {
+    Link.addEventListener("click", closeNavbar);
+  });
+
+  function openNavbar() {
+    header.classList.add("open");
+    scrollTo(0, 0);
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeNavbar() {
+    header.classList.remove("open");
+    document.body.style.overflow = "auto";
+  }
+
+  function handleNavbar() {
+    if (header.classList.contains("open")) {
+      closeNavbar();
+    } else {
+      openNavbar();
+    }
+  }
+
+  btnOpenNav.addEventListener("click", handleNavbar);
+}
+
+const progresses = document.querySelectorAll("[data-progress]");
+
+progresses.forEach((Item) => {
+  Item.style.width = Item.getAttribute("data-progress");
+});
+
+initNavbarAnimation();
+initNavbar();
